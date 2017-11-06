90131b150668075a9fb88c21c82f2b591447fbdc
CharlesNo <charles63500@gmail.com>
2017-11-06 09:43:00 +0000

    Update ibash.sh

--- ibash.sh
+++ ibash.sh
@@ -7,7 +7,6 @@ getStuff(){
     apt-get update -yq && apt-get install -yq curl bash sudo
   fi
   curl -sSL https://raw.githubusercontent.com/CharlesNo/usefulCommands/master/.bashrc > ~/.bashrc
-  . ~/.bashrc
 }
 
 getStuff
