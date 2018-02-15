d4aaabe62359bbca23af8c6f97dad5cd6ecf74a9
Charles N <charlesnowow@gmail.com>
2018-02-15 08:56:13 +0000

    op.sh + exe

---
+++ opi.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir -p /containers/portainer
+
+docker rm -f tumblr
+docker run -d --restart=always --net=host --name tumblr tumblr
+
+docker rm -f test
+docker run -d --restart=always --net=host --name test -v /sys/class/gpio/:/sys/class/gpio/ -v /sys/devices/platform:/sys/devices/platform -p 80:8080  test
+
+docker rm -f netdata
+docker run -d --restart=always --net=host --name netdata --cap-add SYS_PTRACE -v /proc:/host/proc:ro -v /sys:/host/sys:ro -v /var/run/docker.sock:/var/run/docker.sock:ro firehol/netdata:armv7hf
+
+docker rm -f portainer
+docker run -d --restart=always --net=host --name portainer -v /var/run/docker.sock:/var/run/docker.sock -v /containers/portainer:/data --restart=always portainer/portainer:linux-arm
+
