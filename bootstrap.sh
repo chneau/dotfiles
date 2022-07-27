52f923e4f90b09028692b018184775e8d922a1e8
chneau <charles63500@gmail.com>
2022-07-27 12:00:24 +0100

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-url=https://raw.githubusercontent.com/chneau/dotfiles/master
+URL=https://raw.githubusercontent.com/chneau/dotfiles/master
 fetch() {
-    echo "Downloading $url/$1"
+    echo "Downloading $URL/$1 to $HOME/$1"
     if command -v curl >/dev/null 2>&1; then
-        curl -fsSLo "$1" "$url/$1"
+        curl -fsSLo "$HOME/$1" "$URL/$1"
     elif command -v wget >/dev/null 2>&1; then
-        wget -qO "$1" "$url/$1"
+        wget -qO "$HOME/$1" "$URL/$1"
     else
         echo "Neither curl nor wget found. Please install one of these."
         exit 1
