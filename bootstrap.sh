1b95a68b919d7dc15068e106b5ab029bcebba41e
chneau <charles63500@gmail.com>
2022-07-27 03:15:15 +0200

    ok

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-wget -qO ~/.bashrc raw.githubusercontent.com/chneau/dotfiles/master/.bashrc || curl -fsSLo ~/.bashrc raw.githubusercontent.com/chneau/dotfiles/master/.bashrc
+echo "Downloading and installing the latest version of the dotfiles..."
+echo "Downloading .bashrc..."
+wget -qO ~/.bashrc raw.githubusercontent.com/chneau/dotfiles/master/.bashrc ||
+    curl -fsSLo ~/.bashrc raw.githubusercontent.com/chneau/dotfiles/master/.bashrc
+
+echo "Downloading .zshrc..."
+wget -qO ~/.zshrc raw.githubusercontent.com/chneau/dotfiles/master/.zshrc ||
+    curl -fsSLo ~/.zshrc raw.githubusercontent.com/chneau/dotfiles/master/.zshrc
+
+echo "Downloading .aliases..."
+wget -qO ~/.aliases raw.githubusercontent.com/chneau/dotfiles/master/.aliases ||
+    curl -fsSLo ~/.aliases raw.githubusercontent.com/chneau/dotfiles/master/.aliases
+
+echo "Downloading .env..."
+wget -qO ~/.env raw.githubusercontent.com/chneau/dotfiles/master/.env ||
+    curl -fsSLo ~/.env raw.githubusercontent.com/chneau/dotfiles/master/.env
