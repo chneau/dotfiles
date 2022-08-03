2251e0f136cbface82bc38b88cfd26d8ee8aeeb8
chneau <charles63500@gmail.com>
2022-08-03 11:20:08 +0100

    Forcing data refresh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,15 +3,16 @@ URL=https://raw.githubusercontent.com/chneau/dotfiles/master
 fetch() {
     echo "Downloading $URL/$1 to $HOME/$1"
     if command -v curl >/dev/null 2>&1; then
-        curl -fsSLo "$HOME/$1" "$URL/$1"
+        curl -H 'Cache-Control: no-cache, no-store' -fsSLo "$HOME/$1" "$URL/$1?$(date +%s)"
     elif command -v wget >/dev/null 2>&1; then
-        wget -qO "$HOME/$1" "$URL/$1"
+        wget --no-cookies --no-cache -qO "$HOME/$1" "$URL/$1?$(date +%s)"
     else
         echo "Neither curl nor wget found. Please install one of these."
         exit 1
     fi
 }
-fetch .bashrc
-fetch .zshrc
-fetch .aliases
-fetch .profile
+fetch .bashrc &
+fetch .zshrc &
+fetch .aliases &
+fetch .profile &
+wait
